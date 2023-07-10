52236ccebfba725312fd9fe7ddb12a62437e9454
KimWhyt <Klife1889@gmail.com>
2023-07-09 18:35:41 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -prints name of file
+ * @argc: First parameter counts the arguments
+ * @argv: Second parameter is an array of strings
+ * that contains the  actual arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	(void) argv;
+printf("%d\n", argc - 1);
+	return (0);
+}
