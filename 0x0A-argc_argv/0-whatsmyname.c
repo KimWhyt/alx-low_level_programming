e78246e65a671fde7dbb27d6b73dc3edeeb1e994
KimWhyt <Klife1889@gmail.com>
2023-07-09 17:16:29 -0700

    amend  0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main -prints name of file
+ * @argc: First parameter counts the arguments
+ * @argv: Second parameter is an array of strings
+ * that contains the  actual arguments
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
-	(void) argc; 
-
+	(void) argc;
 	printf("%sn", argv[0]);
-	return 0;
+	return (0);
 }
